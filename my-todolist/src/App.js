354f34cd6917eb493e129fd89768b1b6b394cfe3
apuggioni <agustinpuggioni98@gmail.com>
2022-06-29 19:50:40 -0300

    funcionalidad de la app

--- my-todolist/src/App.js
+++ my-todolist/src/App.js
@@ -1,33 +1,14 @@
 import "./App.css";
 import React from "react";
+import TodoList from "./components/TodoList";
 
 //components
-import ToDoList from "./components/TodoList";
-import FormToDo from "./components/FormToDo";
+
 function App() {
-  const [todos, setTodos] = React.useState({
-    text: "",
-    isDone: false,
-  });
-  //functions
-  const addTodo = (text) => {
-    const newTodos = [...todos, { text }];
-    setTodos(newTodos);
-  };
-  const markTodo = (index) => {
-    const newTodos = [...todos];
-    newTodos[index].isDone = true;
-    setTodos(newTodos);
-  };
-  const removeTodo = (index) => {
-    const newTodos = [...todos];
-    newTodos.splice(index, 1);
-    setTodos(newTodos);
-  };
   return (
     <div className="App">
       <h1>To Do List</h1>
-      <FormToDo addTodo={addTodo} />
+      <TodoList></TodoList>
     </div>
   );
 }
--- my-todolist/src/components/TodoList.jsx
+++ my-todolist/src/components/TodoList.jsx
@@ -1,8 +1,54 @@
-import React from "react";
-import ToDo from "./Todo";
+import React, { useState } from "react";
+import Todo from "./Todo";
+import TodoForm from "./TodoForm";
 
-const ToDoList = () => {
-  return <div></div>;
-};
+function TodoList() {
+  const [todos, setTodos] = useState([]);
+  //functions
+  const addTodo = (todo) => {
+    if (!todo.text || /^\s*$/.test(todo.text)) {
+      return;
+    }
 
-export default ToDoList;
+    const newTodos = [todo, ...todos];
+
+    setTodos(newTodos);
+    console.log(...todos);
+  };
+  const updateTodo = (todoId, newValue) => {
+    if (!newValue.text || /^\s*$/.test(newValue.text)) {
+      return;
+    }
+
+    setTodos((prev) =>
+      prev.map((item) => (item.id === todoId ? newValue : item))
+    );
+  };
+  const completeTodo = (id) => {
+    let updatedTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        todo.isComplete = !todo.isComplete;
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
+  const removeTodo = (id) => {
+    const removeArr = [...todos].filter((todo) => todo.id !== id);
+    setTodos(removeArr);
+  };
+  return (
+    <div>
+      <h1> Okay... whtats the plan today?</h1>
+      <TodoForm onSubmit={addTodo}></TodoForm>
+      <Todo
+        todos={todos}
+        completeTodo={completeTodo}
+        removeTodo={removeTodo}
+        updateTodo={updateTodo}
+      ></Todo>
+    </div>
+  );
+}
+
+export default TodoList;
